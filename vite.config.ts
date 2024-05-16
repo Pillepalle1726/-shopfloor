4e7c0b2f097f2ae5f12d7aedb5d101ff4a347113
philipp.michalik <philipp.michalik@exxeta.com>
2024-05-16 18:33:36 +0200

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/build/",
   plugins: [react()],
   assetsInclude: ['**/*.md'],
-  build: {
-    // Specify the directory to output build files
-    outDir: 'build',
-  }
 })
